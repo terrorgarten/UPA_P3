e23ff26d5527c33003c50900551d58f9f05825d8
terrorgarten <xkonop03@vutbr.cz>
2023-12-05 20:40:27 +0100

    changed python -> python3 in bash scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
-python scrape_urls.py > url_test.txt
+python3 scrape_urls.py > url_test.txt
 first_ten="temp_first_10.txt"
 head -n 10 url_test.txt > "$first_ten"
-python scrape_details.py "$first_ten"
+python3 scrape_details.py "$first_ten"
 rm "$first_ten"
